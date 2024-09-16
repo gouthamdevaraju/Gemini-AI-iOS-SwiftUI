e4206b731a0dc46e62cd01b43e57a40c896fbb49
gouthamdevaraju <gouthamdevaraju@gmail.com>
2024-09-16 19:15:14 +0530

    Adding initial project files

---
+++ Gemini-AI-iOS-SwiftUI/Gemini_AI_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemini_AI_iOS_SwiftUIApp.swift
+//  Gemini-AI-iOS-SwiftUI
+//
+//  Created by Goutham Devaraju on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Gemini_AI_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
